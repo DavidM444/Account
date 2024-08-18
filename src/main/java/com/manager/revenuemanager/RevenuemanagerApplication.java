2f4c9c3f2cc3974c28594baa80a68613838f56a7
DavidM444 <david541gamer@gmail.com>
2024-08-17 19:26:41 -0500

    Project Loaded

---
+++ src/main/java/com/manager/revenuemanager/RevenuemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.manager.revenuemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenuemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevenuemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/revenuemanager/RevenuemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.revenuemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevenuemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
