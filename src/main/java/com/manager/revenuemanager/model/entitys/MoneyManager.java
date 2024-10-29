4940809ae632d0f329e11c15f63cd05e5c13bbd5
DavidM444 <david541gamer@gmail.com>
2024-10-28 21:25:21 -0500

    convertoBigDecimal static method with String parameter.

--- src/main/java/com/manager/revenuemanager/model/entitys/MoneyManager.java
+++ src/main/java/com/manager/revenuemanager/model/entitys/MoneyManager.java
@@ -10,15 +10,22 @@ public class MoneyManager {
     //Currency representa la moneda de un pais, con esta clase puedo obtener los decimales, simbolo, codigo(COP) y la instancia de esa moneda.
     //Ademas acepta objeto Locale, para obtener una moneda de acurdo a una localizacion.
     Currency currency = Currency.getInstance("COP");
-    private String saldo;
+    private static String saldo;
     public MoneyManager(String monto){
-        this.saldo = monto;
+        saldo = monto;
     }
 
     public BigDecimal convertoBigDecimal(){
         String valorSinMiles = saldo.replace(".","");
         return new BigDecimal(saldo);
     }
+    public static BigDecimal convertoBigDecimal(String saldo){
+        System.out.println("recibiendo valor a cambiar "+saldo);
+        String valorSinMiles = saldo.replace(".","");
+        System.out.println("valor cambiado: "+valorSinMiles +" "+saldo);
+        return new BigDecimal(saldo);
+    }
+
     public String convertoString(){
         formatSaldo.setCurrency(currency); //definimos el formater con moneda COP
         return formatSaldo.format(saldo);
