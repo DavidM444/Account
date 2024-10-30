7b25823be6be976c9bdec83f3aebf367fb6a1ea8
DavidM444 <david541gamer@gmail.com>
2024-10-29 22:02:28 -0500

    Entity loading.

--- src/main/java/com/manager/revenuemanager/initialize/LoadUserAccountObject.java
+++ src/main/java/com/manager/revenuemanager/initialize/LoadUserAccountObject.java
@@ -5,13 +5,15 @@ import com.manager.revenuemanager.model.entitys.User;
 import com.manager.revenuemanager.model.repositories.AccountRepository;
 import com.manager.revenuemanager.model.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class LoadUserAccountObject {
 
-    private static Account accountDbInstance;
+    private Boolean isDataDbLoaded = false;
 
     @Autowired
     private UserRepository repository;
@@ -20,24 +22,34 @@ public class LoadUserAccountObject {
     private AccountRepository accountRepository;
 
     @PostConstruct
+    @Transactional
     public void init(){
-        if(repository.count()==0 && accountRepository.count()==0){
-            User user = User.getInstance();
-            User user1 = repository.save(user);
-
-            // setear instancia de usuario para trabajar con singleton
-            //User.setUserInstance(user1);
-            Account account = Account.getInstance();
-            account.setUserId(user1);
-            accountDbInstance = accountRepository.save(account);
-            System.out.println("db instance "+accountDbInstance.toString());
+        System.out.println("Valor de account " + Account.getInstance().toString() + "  user account "+ User.getInstance().toString());
+        System.out.println("llamando metodo void");
+        if(accountRepository.count()==0||repository.count()==0){
+            saveUserAndAccountData();
         }
+
+        if(Account.getInstance().getAccount_id() == null || User.getInstance().getUid()==null){
+            loadEntityDb();
+        }
+
+
+    }
+    void saveUserAndAccountData(){
+        User userDb = repository.save(User.getInstance());
+        User.setUserToInstanceDb(userDb);
+
+        // setear instancia de usuario para trabajar con singleton
+        //User.setUserInstance(user1);
+
+        Account accountDbInstance = accountRepository.save(Account.getInstance());
+        Account.setToAccountDbInstance(accountDbInstance);
+        System.out.println("db instance "+accountDbInstance.toString());
     }
 
-    public static Account getAccountDbInstance(){
-        if(accountDbInstance.getUserId().getUid() ==null){
-            throw new RuntimeException("User not Found with ID: " + User.getInstance().getUid());
-        }
-        return accountDbInstance;
+    public void loadEntityDb(){
+        User.setUserToInstanceDb(repository.findByClave(User.getInstance().getClave())) ;
+        Account.setToAccountDbInstance(accountRepository.getAccountDb(User.getInstance().getUid())); ;
     }
 }
