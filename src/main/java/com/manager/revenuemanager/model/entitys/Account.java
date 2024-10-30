78db81afd0b150a380f7645653354c3edd9914ca
DavidM444 <david541gamer@gmail.com>
2024-10-30 11:55:16 -0500

    Handle amount Account.

--- src/main/java/com/manager/revenuemanager/model/entitys/Account.java
+++ src/main/java/com/manager/revenuemanager/model/entitys/Account.java
@@ -2,45 +2,82 @@ package com.manager.revenuemanager.model.entitys;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
-@ToString
+@Setter
 @Entity
 @Table(name = "account")
 public class Account {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private final UUID account_id;
+    private UUID account_id;
 
     @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "uid")
     private User userId;
 
-    private Double amount_account;
+
+    @Setter
+    @Column(precision = 12, scale = 2)
+    private BigDecimal amount_account;
 
 
     private static Account account;
-    public Account(User user, Double amount){
-        this.account_id = UUID.randomUUID();
-        this.userId = user;
-        this.amount_account = amount;
 
+    private static void setAccount(Account account) {
+        Account.account = account;
     }
 
-    @OneToMany(mappedBy = "detailAccountId") //no usar y mejor retonar los datos de forma manual
+    public Account(User user, BigDecimal amount) {
+        this.userId = user;
+        this.amount_account = amount;
+    }
+
+    public Account() {
+    }
+
+    @OneToMany(mappedBy = "detailAccountId", fetch = FetchType.LAZY) //no usar y mejor retonar los datos de forma manual
     List<Detail> listDetails = new LinkedList<>();
 
-    public static Account getInstance(){
-        if(account ==null){
-            return new Account(User.getInstance(), 100.000);
+    public static Account getInstance() {
+        if (account == null) {
+            account = new Account(User.getInstance(), new BigDecimal("0"));
         }
         return account;
     }
+
+    public static void setToAccountDbInstance(Account accountDb) {
+        if (account.getAccount_id() == null) {
+            setAccount(accountDb);
+        }
+    }
+
+    void addSaldo(BigDecimal saldo) {
+        account.setAmount_account(account.getAmount_account().add(saldo));
+        System.out.println("saldo actual en addSaldo: " + saldo + "--- " + "   tot " + (account.getAmount_account()));
+    }
+
+    void sacarSaldo(BigDecimal saldo) {
+        BigDecimal saldoActual = account.getAmount_account();
+        account.setAmount_account(account.getAmount_account().subtract(saldo));
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "account_id=" + account_id +
+                ", amount_account=" + amount_account +
+                '}';
+    }
+
 }
