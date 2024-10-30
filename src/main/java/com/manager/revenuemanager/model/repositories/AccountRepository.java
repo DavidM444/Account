c7babccd3bde23cd2980d94e6ffaa4b6e63a4e0b
DavidM444 <david541gamer@gmail.com>
2024-10-29 22:07:28 -0500

    Methods with specific queries.

--- src/main/java/com/manager/revenuemanager/model/repositories/AccountRepository.java
+++ src/main/java/com/manager/revenuemanager/model/repositories/AccountRepository.java
@@ -1,12 +1,15 @@
 package com.manager.revenuemanager.model.repositories;
 
+import com.manager.revenuemanager.initialize.LoadUserAccountObject;
 import com.manager.revenuemanager.model.entitys.Account;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface AccountRepository extends CrudRepository<Account, UUID> {
@@ -19,4 +22,15 @@ public interface AccountRepository extends CrudRepository<Account, UUID> {
 //    @Query("SELECT COUNT(a) FROM Account a")
 //    @Transactional
 //    long count();
+
+    // crear metodo para que actualice el valor del amount de la cuenta luego de cada agregacion de un detail.
+    @Modifying //indica que es una cosulta como update o delete
+    @Query("UPDATE Account a SET a.amount_account = :amount WHERE a.account_id = :id")
+    @Transactional
+    void setAmountById(@Param("amount")BigDecimal amount, @Param("id") UUID id);
+
+    @Query("SELECT a FROM Account a WHERE a.userId.uid = :userid")
+    @Transactional(readOnly = true)
+    Account getAccountDb(@Param("userid") UUID userid);
+
 }
