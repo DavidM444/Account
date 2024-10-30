1872ce00ac4c5cb0995acf8a8f06cee431986869
DavidM444 <david541gamer@gmail.com>
2024-10-29 22:01:00 -0500

    BigDecimal value formmater enhanced.

--- src/main/java/com/manager/revenuemanager/model/entitys/MoneyManager.java
+++ src/main/java/com/manager/revenuemanager/model/entitys/MoneyManager.java
@@ -6,10 +6,10 @@ import java.util.Currency;
 import java.util.Locale;
 
 public class MoneyManager {
-    NumberFormat formatSaldo = NumberFormat.getCurrencyInstance( new Locale("es","co"));
+    static NumberFormat formatSaldo = NumberFormat.getCurrencyInstance( new Locale("es","CO"));
     //Currency representa la moneda de un pais, con esta clase puedo obtener los decimales, simbolo, codigo(COP) y la instancia de esa moneda.
     //Ademas acepta objeto Locale, para obtener una moneda de acurdo a una localizacion.
-    Currency currency = Currency.getInstance("COP");
+    static Currency currency = Currency.getInstance("COP");
     private static String saldo;
     public MoneyManager(String monto){
         saldo = monto;
@@ -17,18 +17,21 @@ public class MoneyManager {
 
     public BigDecimal convertoBigDecimal(){
         String valorSinMiles = saldo.replace(".","");
-        return new BigDecimal(saldo);
+        return new BigDecimal(valorSinMiles);
     }
     public static BigDecimal convertoBigDecimal(String saldo){
         System.out.println("recibiendo valor a cambiar "+saldo);
         String valorSinMiles = saldo.replace(".","");
+
         System.out.println("valor cambiado: "+valorSinMiles +" "+saldo);
         return new BigDecimal(saldo);
     }
 
-    public String convertoString(){
-        formatSaldo.setCurrency(currency); //definimos el formater con moneda COP
-        return formatSaldo.format(saldo);
+    public static String convertoString(BigDecimal bigDecimal){
+        formatSaldo.setCurrency(currency);
+     //definimos el formateador con moneda COP
+        String formmatedBalance =formatSaldo.format(bigDecimal);
+        return formmatedBalance.replace("$","");
     }
 
 }
