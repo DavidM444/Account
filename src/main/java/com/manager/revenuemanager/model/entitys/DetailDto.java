78daeaaf7645a16b57f498f36e0317886e7b8a17
DavidM444 <david541gamer@gmail.com>
2024-11-05 18:13:07 -0500

    description attribute to Dto

--- src/main/java/com/manager/revenuemanager/model/entitys/DetailDto.java
+++ src/main/java/com/manager/revenuemanager/model/entitys/DetailDto.java
@@ -1,8 +1,10 @@
 package com.manager.revenuemanager.model.entitys;
 
 public class DetailDto {
+
+    private String description;
     private String tipo;
-    private String amount;
+    private String amountHistory;
 
     public String getTipo() {
         return tipo;
@@ -12,19 +14,28 @@ public class DetailDto {
         this.tipo = tipo;
     }
 
-    public String getAmount() {
-        return amount;
+    public String getAmountHistory() {
+        return amountHistory;
     }
 
-    public void setAmount(String amount){
-        this.amount = amount;
+    public void setAmountHistory(String amountHistory){
+        this.amountHistory = amountHistory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public DetailDto(){}
 
-    public DetailDto(String tipo, String amount){
+    public DetailDto(String description,String tipo, String amount){
         super();
         this.tipo = tipo;
-        this.amount = amount;
+        this.amountHistory = amount;
+        this.description = description;
     }
 }
